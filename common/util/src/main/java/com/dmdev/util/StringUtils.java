a2ea691096037bd6cf2c7b74e80be618fc18f24b
Anton Pashkin <anton.zep@gmail.com>
2023-01-28 13:15:53 +0300

    added common:util project files in multiproject build

---
+++ common/util/src/main/java/com/dmdev/util/StringUtils.java
@@ -0,0 +1,13 @@
+package com.dmdev.util;
+
+public final class StringUtils
+{
+    private static final String EMPTY = "";
+
+    private StringUtils() {}
+
+    public static String trim(String value)
+    {
+        return value == null ? EMPTY : value.trim();
+    }
+}
